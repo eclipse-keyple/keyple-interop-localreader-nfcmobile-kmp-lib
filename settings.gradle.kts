c3e6e346f7ca2be57edf2f0616ba890c7104353e
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-05-27 12:07:21 +0200

    feat(build): add Maven snapshots repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,6 +27,7 @@ dependencyResolutionManagement {
                 includeGroupAndSubgroups("com.google")
             }
         }
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
